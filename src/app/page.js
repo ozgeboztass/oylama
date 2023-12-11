5d86ffedc97afe7fcc151fe679ffa4e58876e0b3
ozgeboztass <123666735+ozgeboztass@users.noreply.github.com>
2023-12-11 20:50:29 +0300

    *****

---
+++ src/app/page.js
@@ -0,0 +1,80 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <section className="text-gray-400 body-font justify-between relative">
+      <div className="container  mx-auto flex justify-center items-center ">
+        <div className="lg:w-1/3 md:w-1/2 bg-gray-900 shadow-md rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10">
+          <h2 className="text-white text-lg mb-1 font-medium title-font">
+            Feedback
+          </h2>
+          <p className="leading-relaxed mb-5">
+            Post-ironic portland shabby chic echo park, banjo fashion axe
+          </p>
+
+          <button className="text-white bg-purple-500 border-0 py-2 px-6 focus:outline-none hover:bg-purple-600 rounded text-lg">
+            Button
+          </button>
+          <button className="text-white mt-5 bg-purple-500 border-0 py-2 px-6 focus:outline-none hover:bg-purple-600 rounded text-lg">
+            Button
+          </button>
+          <button className="text-white mt-5 bg-purple-500 border-0 py-2 px-6 focus:outline-none hover:bg-purple-600 rounded text-lg">
+            Button
+          </button>
+          <p className="text-xs text-gray-400 text-opacity-90 mt-3">
+            Chicharrones blog helvetica normcore iceland tousled brook viral
+            artisan.
+          </p>
+        </div>
+      </div>
+      <div className="container mx-auto flex justify-center items-center min-h-screen">
+        <div className="lg:w-1/3 md:w-1/2 bg-gray-900 shadow-md rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10">
+          <h2 className="text-white text-lg mb-1 font-medium title-font">
+            Feedback
+          </h2>
+          <p className="leading-relaxed mb-5">
+            Post-ironic portland shabby chic echo park, banjo fashion axe
+          </p>
+
+          <button className="text-white bg-purple-500 border-0 py-2 px-6 focus:outline-none hover:bg-purple-600 rounded text-lg">
+            Button
+          </button>
+          <button className="text-white mt-5 bg-purple-500 border-0 py-2 px-6 focus:outline-none hover:bg-purple-600 rounded text-lg">
+            Button
+          </button>
+          <button className="text-white mt-5 bg-purple-500 border-0 py-2 px-6 focus:outline-none hover:bg-purple-600 rounded text-lg">
+            Button
+          </button>
+          <p className="text-xs text-gray-400 text-opacity-90 mt-3">
+            Chicharrones blog helvetica normcore iceland tousled brook viral
+            artisan.
+          </p>
+        </div>
+      </div>
+      <div className="container mx-auto flex justify-center items-center min-h-screen">
+        <div className="lg:w-1/3 md:w-1/2 bg-gray-900 shadow-md rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10">
+          <h2 className="text-white text-lg mb-1 font-medium title-font">
+            Feedback
+          </h2>
+          <p className="leading-relaxed mb-5">
+            Post-ironic portland shabby chic echo park, banjo fashion axe
+          </p>
+
+          <button className="text-white bg-purple-500 border-0 py-2 px-6 focus:outline-none hover:bg-purple-600 rounded text-lg">
+            Button
+          </button>
+          <button className="text-white mt-5 bg-purple-500 border-0 py-2 px-6 focus:outline-none hover:bg-purple-600 rounded text-lg">
+            Button
+          </button>
+          <button className="text-white mt-5 bg-purple-500 border-0 py-2 px-6 focus:outline-none hover:bg-purple-600 rounded text-lg">
+            Button
+          </button>
+          <p className="text-xs text-gray-400 text-opacity-90 mt-3">
+            Chicharrones blog helvetica normcore iceland tousled brook viral
+            artisan.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
